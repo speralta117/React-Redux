140c6769f4fb070effde790d706110a0b62c99db
Salvador Peralta <salvadorperalta@MacBookProSpc.local>
2018-12-27 22:39:44 -0600

    Ajax status, Display user notifications, handling errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 import Header from './common/Header';
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-       <Header/>
+       <Header
+          loading={this.props.loading}
+        />
         {this.props.children}
       </div>
     );
@@ -14,7 +18,14 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
